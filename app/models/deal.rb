8cd15cbcadc2ee6e0470671a5ef941dbcb1a2892
Jonathan To <jonto94@gmail.com>
2023-05-04 17:07:49 +1000

    updated deals model and index

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,7 +1,7 @@
 class Deal < ApplicationRecord
   belongs_to :business
   belongs_to :user
-  validates :name, :category, :unit, :price, :description, :submission_date, presence: true
+  validates :name, :category, :price, :description, :submission_date, presence: true
   has_one_attached :image
   has_many :recipes, through: :recipe_deals
 end
