0a071af26c5b73a5bb2214d2c375d13fe6b6476f
Allan Turton <forshopping50@hotmail.com>
2023-05-08 23:20:29 +1000

    Currently stuck on distance_to method to display nearby businesses

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home choice]
+  # AT: added to try out geocoding 08/05
+  before_action :business_list, only: [:choice]
+
 
   def home
   end
@@ -14,4 +17,11 @@ class PagesController < ApplicationController
       }
     end
   end
+
+  def business_list
+    @melbourne_location = Geocoder.search('Melbourne, VIC, Australia').first
+    @targeted_businesses = Business.near([@melbourne_location.latitude, @melbourne_location.longitude], 20, units: :km)
+    puts "Melbourne location: lat=#{@melbourne_location.latitude}, lng=#{@melbourne_location.longitude}"
+  end
+
 end
