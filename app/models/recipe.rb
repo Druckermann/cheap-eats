cafbd275557ba0bdb9bfed6e3c79d5794e75b6c3
samira hossain <samira.hossain@outlook.com>
2023-05-09 19:58:10 +1000

    Added favorite to recipe/user and deleted favorite.rb model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,9 @@
 class Recipe < ApplicationRecord
+  acts_as_favoritable
+
   validates :name, :servings, :cook_time, :prep_time, :description, :ingredients, :steps, presence: true
   has_one_attached :image
   has_many :deals, through: :recipe_deals
-
-  has_many :favourites
   has_many :users, through: :favourites
 end
 
