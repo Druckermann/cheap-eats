3530419bcd605a668c982f838a600312113903e6
Allan Turton <forshopping50@hotmail.com>
2023-05-09 23:50:53 +1000

    Search box off map.

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -17,8 +17,11 @@ export default class extends Controller {
     });
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
-    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl }))
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+      });
+    document.getElementById('geocoder').appendChild(geocoder.onAdd(this.map));
 
   }
 
