7e3adb1f4f8b03d08ecaae0693eb5ef052ce129a
Allan Turton <forshopping50@hotmail.com>
2023-05-19 16:25:06 +1000

    Main navbar updated to match style in Figma. Dropdowns neater but still require extra styling.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # AT: Added 19/05/23 due to repeated rails assets:precompile
+  config.assets.debug = true
+
 end
